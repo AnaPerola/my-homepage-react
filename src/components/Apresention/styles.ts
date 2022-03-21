f16e2a454e9aa9db1a5a586433423f43cbb0e642
Ana Perola <anaperolinha@hotmail.com>
2022-03-20 23:49:52 -0300

    Create new component and style Apresention

---
+++ src/components/Apresention/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  max-width: 498px;
+  height: 100vh;
+  padding: 2.5rem 1rem;
+  background: #a73434;
+`
+
+export const Img = styled.img`
+
+`
+export const Text = styled.h2`
+  color: var(--white-text);
+  font-size: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+
+`
