988e0bc0f303d6627998c89902d1a260b031d7f3
Ana Perola <anaperolinha@hotmail.com>
2022-03-20 23:50:24 -0300

    Create new component and style Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Content } from "./styles";
+
+export function Header(){
+  return (
+    <Container>
+      <Content>
+        header
+      </Content>
+    </Container>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background: var(--black);
+`
+
+export const Content = styled.div`
+  display: flex;
+  padding: 13px 21px 15px 13px;
+  color: var(--white-text);
+  font-family: Arial, Helvetica, sans-serif;
+`
